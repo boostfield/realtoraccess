9b246d64723a892af9c5a3e572ca5b7808dc0585
qky <qky1412@gmail.com>
2018-05-02 17:25:55 +0800

    基本完成注册、登录、经纪主页框架

--- main.js
+++ main.js
@@ -6,18 +6,66 @@ $(document).ready(function () {
     invalidHandler: function(event, validator) {
     }
   })
+  $("#formRegister").validate({
+    submitHandler: function(form) {
+      register()
+    },
+    invalidHandler: function(event, validator) {
+    }
+  })
+  $('span.service').popover();
+  $('.read-more').popover();
+  $('.btn-option.wechat').hover(function () {
+      $('#wechat-qrcode').css("display","block");
+  },function(){
+    $('#wechat-qrcode').css("display","none");
+  })
+  $('.backTop').click(function () {
+      $('html,body').animate({ scrollTop: 0 }, 'slow');
+  })
 })
 
-function login() {
-  var userid = $('#userid').val()
-  var passwd = $('#passwd').val()
-  var data = {userid: userid, passwd: passwd}
-  $.post('http://www.realtoraccess.com/web/signin/', data, function(resp){
-    console.log(resp)
-    if (resp.rescode != 0) {
-      console.log('用户名或密码错误')
-    } else {
-      console.log('登录成功')
+function login () {
+  let userid = $('#userid').val()
+  let passwd = $('#passwd').val()
+  let data = {userid: userid, passwd: passwd}
+  $.ajax({
+    type: 'POST',
+    url: 'http://www.realtoraccess.com/web/signin/',
+    data: data,
+    dataType: 'JSON',
+    success: function(resp) {
+      console.log(resp)
+      let code = resp.rescode
+      console.log(code)
+      if (code != "0") {
+        console.log('用户名或密码错误')
+      } else {
+        console.log('登录成功')
+      }
     }
-	})
+    
+  })
+}
+
+function register () {
+  let userid = $('#userid').val()
+  let passwd = $('#passwd').val()
+  let data = {userid: userid, passwd1: passwd, passwd2: passwd}
+  $.ajax({
+    type: 'POST',
+    url: 'http://www.realtoraccess.com/web/signup/',
+    data: data,
+    dataType: 'JSON',
+    success: function(resp) {
+      console.log(resp)
+      let code = resp.rescode
+      if (code != 0) {
+        if (code == 1)
+        console.log('用户已存在')
+      } else {
+        console.log('注册成功')
+      }
+    }
+  })
 }
